109c157d8b43d99d678dfa29b994f9ee34a5c722
lobothiago <lobo.alvesthiago@gmail.com>
2018-07-11 03:07:10 -0300

    Criando insert

--- Crud/resources/views/welcome.blade.php
+++ Crud/resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
-<@section('content')
-    <h1>Funcionou</h1>
-@endsection
+@section('content')
+    <div class="container">
+        @if (session('successMsg'))
+            <div class="alert alert-dismissible alert-success">
+              <button type="button" class="close" data-dismiss="alert">×</button>
+              <strong>Well done!</strong> {{ session('successMsg') }}
+            </div>
+        @endif
+  
+        @endsection        
--- Crud/routes/web.php
+++ Crud/routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','StudentController@index')->name('home');
+Route::get('create','StudentController@create')->name('create');
+Route::post('create','StudentController@store')->name('store');
